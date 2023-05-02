efd3715c972f466c6c1ec3ba86f98dff54978e0f
Hayate Nakano <nakanodesu@gmail.com>
2023-05-02 23:33:54 +0900

    add

--- src/binop.rs
+++ src/binop.rs
@@ -4,24 +4,77 @@ use super::util::*;
 use nom::{
     bytes::complete::tag,
     character::complete::{char, one_of},
+    combinator::map,
+    branch::alt,
     IResult,
 };
 
-pub fn take_sub(s: &str) -> IResult<&str, Token> {
-    max_space_around(s, char('_')).map(|(s, n)| (s, Token::Sub(n)))
+pub fn take_binop(s: &str) -> IResult<&str, Token> {
+    alt((take_over, take_under, take_sub, take_sup, take_frac, take_cat))(s)
 }
-pub fn take_under(s: &str) -> IResult<&str, Token> {
-    max_space_around(s, tag("__")).map(|(s, n)| (s, Token::Under(n)))
+
+fn take_sub(s: &str) -> IResult<&str, Token> {
+    map(count_space_around(char('_')), |x| Token::Sub(x))(s)
 }
-pub fn take_sup(s: &str) -> IResult<&str, Token> {
-    max_space_around(s, char('^')).map(|(s, n)| (s, Token::Sup(n)))
+fn take_under(s: &str) -> IResult<&str, Token> {
+    map(count_space_around(tag("__")), |x| Token::Under(x))(s)
 }
-pub fn take_over(s: &str) -> IResult<&str, Token> {
-    max_space_around(s, tag("^^")).map(|(s, n)| (s, Token::Over(n)))
+fn take_sup(s: &str) -> IResult<&str, Token> {
+    map(count_space_around(char('^')), |x| Token::Sup(x))(s)
 }
-pub fn take_frac(s: &str) -> IResult<&str, Token> {
-    max_space_around(s, one_of("/∕")).map(|(s, n)| (s, Token::Frac(n)))
+fn take_over(s: &str) -> IResult<&str, Token> {
+    map(count_space_around(tag("^^")), |x| Token::Over(x))(s)
 }
-pub fn take_cat(s: &str) -> IResult<&str, Token> {
-    num_space(s).map(|(s, n)| (s, Token::Cat(n)))
+fn take_frac(s: &str) -> IResult<&str, Token> {
+    map(count_space_around(one_of("/∕")), |x| Token::Frac(x))(s)
+}
+fn take_cat(s: &str) -> IResult<&str, Token> {
+    map(num_space, |n| Token::Cat(n))(s)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test() {
+        assert_eq!(take_binop("  _  123").unwrap(), ("123", Token::Sub(2)));
+        assert_eq!(take_binop("  _ a").unwrap(), ("a", Token::Sub(2)));
+        assert_eq!(take_binop("_  <a>").unwrap(), ("<a>", Token::Sub(2)));
+        assert_eq!(take_binop("    _+").unwrap(), ("+", Token::Sub(4)));
+        assert_eq!(take_binop("  _  123").unwrap(), ("123", Token::Sub(2)));
+        assert_eq!(take_binop("_x").unwrap(), ("x", Token::Sub(0)));
+
+        assert_eq!(take_binop("  ^  123").unwrap(), ("123", Token::Sup(2)));
+        assert_eq!(take_binop("  ^ a").unwrap(), ("a", Token::Sup(2)));
+        assert_eq!(take_binop("^  <a>").unwrap(), ("<a>", Token::Sup(2)));
+        assert_eq!(take_binop("    ^+").unwrap(), ("+", Token::Sup(4)));
+        assert_eq!(take_binop("  ^  123").unwrap(), ("123", Token::Sup(2)));
+        assert_eq!(take_binop("^x").unwrap(), ("x", Token::Sup(0)));
+
+        assert_eq!(take_binop("  __  123").unwrap(), ("123", Token::Under(2)));
+        assert_eq!(take_binop("  __ a").unwrap(), ("a", Token::Under(2)));
+        assert_eq!(take_binop("__  <a>").unwrap(), ("<a>", Token::Under(2)));
+        assert_eq!(take_binop("    __+").unwrap(), ("+", Token::Under(4)));
+        assert_eq!(take_binop("  __  123").unwrap(), ("123", Token::Under(2)));
+        assert_eq!(take_binop("__x").unwrap(), ("x", Token::Under(0)));
+
+        assert_eq!(take_binop("  ^^  123").unwrap(), ("123", Token::Over(2)));
+        assert_eq!(take_binop("  ^^ a").unwrap(), ("a", Token::Over(2)));
+        assert_eq!(take_binop("^^  <a>").unwrap(), ("<a>", Token::Over(2)));
+        assert_eq!(take_binop("    ^^+").unwrap(), ("+", Token::Over(4)));
+        assert_eq!(take_binop("  ^^  123").unwrap(), ("123", Token::Over(2)));
+        assert_eq!(take_binop("^^x").unwrap(), ("x", Token::Over(0)));
+
+        assert_eq!(take_binop("  /  123").unwrap(), ("123", Token::Frac(2)));
+        assert_eq!(take_binop("  ∕ a").unwrap(), ("a", Token::Frac(2)));
+        assert_eq!(take_binop("/  <a>").unwrap(), ("<a>", Token::Frac(2)));
+        assert_eq!(take_binop("    /+").unwrap(), ("+", Token::Frac(4)));
+        assert_eq!(take_binop("  /  123").unwrap(), ("123", Token::Frac(2)));
+        assert_eq!(take_binop("/x").unwrap(), ("x", Token::Frac(0)));
+
+        assert_eq!(take_binop(" 123").unwrap(), ("123", Token::Cat(1)));
+        assert_eq!(take_binop("  a").unwrap(), ("a", Token::Cat(2)));
+        assert_eq!(take_binop("   <a>").unwrap(), ("<a>", Token::Cat(3)));
+        assert_eq!(take_binop("+").unwrap(), ("+", Token::Cat(0)));
+    }
 }
--- src/util.rs
+++ src/util.rs
@@ -1,17 +1,20 @@
-use nom::{
-    character::complete::char, error::ParseError, multi::fold_many0, sequence::tuple, IResult,
-    Parser,
-};
+use nom::{character::complete::char, error::ParseError, multi::fold_many0, IResult, Parser};
 
 pub fn num_space<'a, E: ParseError<&'a str>>(s: &'a str) -> IResult<&'a str, usize, E> {
-    fold_many0(char(' '), || 0, |x, _| x + 1)(s)
+    fold_many0(char(' '), usize::default, |x, _| x + 1)(s)
 }
 
-pub fn max_space_around<'a, R, F, E>(s: &'a str, parser: F) -> IResult<&'a str, usize, E>
+pub fn count_space_around<'a, R, F, E>(
+    mut parser: F,
+) -> impl FnMut(&'a str) -> IResult<&'a str, usize, E>
 where
     F: Parser<&'a str, R, E>,
     E: ParseError<&'a str>,
 {
-    let (s, (left, _, right)) = tuple((num_space, parser, num_space))(s)?;
-    Ok((s, left.max(right)))
+    move |input| {
+        let (input, left) = num_space(input)?;
+        let (input, _) = parser.parse(input)?;
+        let (input, right) = num_space(input)?;
+        Ok((input, left.max(right)))
+    }
 }
