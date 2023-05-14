44a73ab613b83e0300f02d2d06c37dea44ceaed8
Hayate Nakano <nakanodesu@gmail.com>
2023-05-14 15:36:46 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 pub mod binop;
+pub mod expr;
 pub mod open_close;
 pub mod symbol;
 pub mod token;
@@ -11,7 +12,9 @@ use nom::combinator::{eof, not};
 use nom::multi::many0;
 use nom::sequence::{preceded, terminated};
 use nom::IResult;
+use unicode_normalization::UnicodeNormalization;
 
+use expr::*;
 use token::Token;
 
 use binop::take_binop;
@@ -20,8 +23,12 @@ use symbol::take_symbol;
 use unaryop::take_op;
 use unicode_subsup::{take_unicode_sub, take_unicode_sup};
 
-fn tokenize(s: &str) -> IResult<&str, Vec<Token>> {
-    terminated(
+fn tokenize(s: &str) -> Result<Vec<Token>, ()> {
+    // normalize
+    let s = s.nfd().to_string();
+    let s = s.trim();
+    // tokenize
+    let (_, t) = terminated(
         many0(preceded(
             not(eof),
             alt((
@@ -35,19 +42,104 @@ fn tokenize(s: &str) -> IResult<&str, Vec<Token>> {
             )),
         )),
         eof,
-    )(s)
+    )(&s)
+    .map_err(|_| ())?;
+    // remove unicode sub/sup
+    let mut t2 = vec![];
+    enum Mode {
+        Sup,
+        Sub,
+        Normal,
+    }
+    let mut mode = Mode::Normal;
+    for x in t {
+        match x {
+            Token::UnicodeSub(y) => {
+                match mode {
+                    Mode::Normal => {
+                        t2.push(Token::Sub(0));
+                        t2.push(Token::Open(".".to_string()));
+                    }
+                    Mode::Sub => {}
+                    Mode::Sup => {
+                        t2.push(Token::Close(".".to_string()));
+                        t2.push(Token::Sub(0));
+                        t2.push(Token::Open(".".to_string()));
+                    }
+                };
+                t2.push(*y);
+                mode = Mode::Sub;
+            }
+            Token::UnicodeSup(y) => {
+                match mode {
+                    Mode::Normal => {
+                        t2.push(Token::Sup(0));
+                        t2.push(Token::Open(".".to_string()));
+                    }
+                    Mode::Sub => {
+                        t2.push(Token::Close(".".to_string()));
+                        t2.push(Token::Sup(0));
+                        t2.push(Token::Open(".".to_string()));
+                    }
+                    Mode::Sup => {}
+                };
+                t2.push(*y);
+                mode = Mode::Sup;
+            }
+            _ => {
+                match mode {
+                    Mode::Sub | Mode::Sup => t2.push(Token::Close(".".to_string())),
+                    Mode::Normal => {}
+                }
+                t2.push(x);
+                mode = Mode::Normal;
+            }
+        };
+    }
+    match mode {
+        Mode::Sub | Mode::Sup => t2.push(Token::Close(".".to_string())),
+        Mode::Normal => {}
+    }
+    // insert Cat(0) between adjacent symbol
+    let mut t3 = vec![];
+    let mut after_symbol = false;
+    for x in t2 {
+        match x {
+            Token::Symbol(_) => {
+                if after_symbol {
+                    t3.push(Token::Cat(0));
+                }
+                t3.push(x);
+                after_symbol = true;
+            }
+            Token::Open(_) => {
+                if after_symbol {
+                    t3.push(Token::Cat(0));
+                }
+                t3.push(x);
+                after_symbol = false;
+            }
+            Token::Close(_) => {
+                t3.push(x);
+                after_symbol = true;
+            }
+            _ => {
+                t3.push(x);
+                after_symbol = false;
+            }
+        }
+    }
+    Ok(t3)
 }
 
-struct Expr {}
-
-fn parse(_: Vec<Token>) -> IResult<Vec<Token>, Expr> {
+fn parse(_: &[Token]) -> IResult<&[Token], Expr> {
     todo!()
 }
 
 fn main() {
     println!(
         "{:#?}",
-        tokenize(r"a + b/c <alpha>[<beta hat>^2] `(X)`   5_/ 123")
+        tokenize(r"a + bᵃ⁺ᵇ⁼ᶜₕₒ/c <alpha>[<beta hat>^2] `(X)`   5_/ 123")
     );
     //parse(vec![]).unwrap();
 }
