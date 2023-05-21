5d4b2e4f54b720d66f9c74c6d6550428457dac75
Hayate Nakano <nakanodesu@gmail.com>
2023-05-21 14:04:34 +0900

    impl

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ pub mod token;
 use expr::parse;
 use token::tokenize;
 
-pub fn maspace(input: &str) -> Result<String, String> {
+pub fn maspace_to_tex(input: &str) -> Result<String, String> {
     parse(&tokenize(input).map_err(|x| format!("{:?}", x))?)
         .map(|x| x.to_string())
         .map_err(|x| format!("{:?}", x))
@@ -12,11 +12,10 @@ pub fn maspace(input: &str) -> Result<String, String> {
 
 fn main() {
     println!(
-        "{:#?}",
-        tokenize(r"a + bᵃ⁺ᵇ⁼ᶜₕₒ/c <alpha>[<beta hat>^2] `(X)`   5_/ 123")
-    );
-    println!(
         "{}",
-        parse(&tokenize(r"a + bᵃ⁺ᵇ⁼ᶜₕₒ/c <alpha>[<beta hat>^2] `(X)`   5_/ 123").unwrap()).unwrap()
+        maspace_to_tex(r"a + bᵃ⁺ᵇ⁼ᶜₕₒ/c <alpha>[<beta hat>^2] `(X)`   5_/ 123").unwrap()
     );
+    println!("{}", maspace_to_tex(r"a _b_c  ^d ^e+f _g  /h").unwrap());
+    println!("{}", maspace_to_tex(r"a _b_c^d ^[e+f _g/h]").unwrap());
+    println!("{}", maspace_to_tex(r"a  _b _c^d  ^e  +  f_g/h").unwrap());
 }
