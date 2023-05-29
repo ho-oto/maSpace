f6de4bc9caddfba843eb5378111841319a886513
Hayate Nakano <nakanodesu@gmail.com>
2023-05-29 22:08:02 +0900

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -30,19 +30,19 @@ mod tests {
     fn test() {
         assert_eq!(
             maspace_to_tex(r"a + bᵃ⁺ᵇ⁼ᶜₕₒ/c <alpha>[<beta hat>^2] `(X)`   5_/ 123").unwrap(),
-            r"a+\frac{{b}^{\left.a+b=c\right.}_{\left.ho\right.}}{c}\alpha\left.{\hat{\beta}}^{2}\right.\otimes\sqrt[5]{123}"
+            r"a + \frac{{b}^{\left.a + b = c\right.}_{\left.h o\right.}}{c} \alpha \left.{\hat{\beta}}^{2}\right. \otimes \sqrt[5]{1 2 3}"
         );
         assert_eq!(
             maspace_to_tex(r"a _b_c  ^d ^e+f _g  /h").unwrap(),
-            r"\frac{{{a}_{{b}_{c}}}^{{d}^{e+f}_{g}}}{h}"
+            r"\frac{{{a}_{{b}_{c}}}^{{d}^{e + f}_{g}}}{h}"
         );
         assert_eq!(
             maspace_to_tex(r"a _b_c^d ^[e+f _g/h]").unwrap(),
-            r"{a}^{\left.{e+f}_{\frac{g}{h}}\right.}_{{b}^{d}_{c}}"
+            r"{a}^{\left.{e + f}_{\frac{g}{h}}\right.}_{{b}^{d}_{c}}"
         );
         assert_eq!(
             maspace_to_tex(r"a  _b _c^d  ^e  +  f_g/h").unwrap(),
-            r"{a}^{e}_{{b}_{{c}^{d}}}+\frac{{f}_{g}}{h}"
+            r"{a}^{e}_{{b}_{{c}^{d}}} + \frac{{f}_{g}}{h}"
         );
     }
 }
