29fcf262fef5149d29190288d5750a39fcb97f18
ho-oto <ho-oto@users.noreply.github.com>
2023-06-04 13:30:51 +0000

    deploy: 65c40b1578de6d62a0c35fcfe6ab2864db2988d2

---
+++ snippets/yew-app-80897f402006c7b2/inline0.js
@@ -0,0 +1,8 @@
+
+import "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg-full.js"
+export function tex2svg(tex_input) {
+    return MathJax.tex2svgPromise(tex_input).then(function (node) {
+        const adaptor = MathJax.startup.adaptor;
+        return adaptor.innerHTML(node);
+    });
+}
