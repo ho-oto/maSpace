b5326c6e2d2c9412275f10b1a6215a04e43e18bb
ho-oto <ho-oto@users.noreply.github.com>
2023-06-05 13:46:04 +0000

    deploy: ef8fd5557e059c76595a147b5c6cbaa2e1e7b013

--- dist/snippets/yew-app-80897f402006c7b2/inline0.js
+++ dist/snippets/yew-app-80897f402006c7b2/inline0.js
@@ -3,6 +3,6 @@ import "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg-full.js"
 export function tex2svg(tex_input) {
     return MathJax.tex2svgPromise(tex_input, {display: true}).then(function (node) {
         const adaptor = MathJax.startup.adaptor;
-        return adaptor.innerHTML(node);
+        return adaptor.outerHTML(node);
     });
 }
--- yew-app-c015f2dfdafc1708.js
+++ yew-app-c015f2dfdafc1708.js
@@ -683,7 +683,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('yew-app-85a5517f55e133fa_bg.wasm', import.meta.url);
+        input = new URL('yew-app-c015f2dfdafc1708_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
