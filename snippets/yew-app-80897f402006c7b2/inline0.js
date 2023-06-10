413f1e98202c28c49ae1373612594f5241847a3d
ho-oto <ho-oto@users.noreply.github.com>
2023-06-10 06:24:31 +0000

    deploy: df6e747f2431bc6e3cba7428871c26c726e312ca

--- snippets/yew-app-80897f402006c7b2/inline0.js
+++ snippets/yew-app-80897f402006c7b2/inline0.js
@@ -1,5 +1,5 @@
 
-import "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg-full.js"
+import "https://cdn.jsdelivr.net/npm/mathjax@3.2.2/es5/tex-svg-full.js"
 export function tex2svg(tex_input) {
     return MathJax.tex2svgPromise(tex_input, {display: true}).then(function (node) {
         const adaptor = MathJax.startup.adaptor;
