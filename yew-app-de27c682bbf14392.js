22eda3c3a0585480e5e5903f6d552ac395b9d412
ho-oto <ho-oto@users.noreply.github.com>
2023-06-10 16:26:51 +0000

    deploy: 067d758ddc184c58333a9e898c30e56c59013399

--- yew-app-de27c682bbf14392.js
+++ yew-app-de27c682bbf14392.js
@@ -636,12 +636,12 @@ function __wbg_get_imports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper491 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 196, __wbg_adapter_18);
+    imports.wbg.__wbindgen_closure_wrapper499 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 198, __wbg_adapter_18);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper836 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 323, __wbg_adapter_21);
+    imports.wbg.__wbindgen_closure_wrapper844 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 325, __wbg_adapter_21);
         return addHeapObject(ret);
     };
 
@@ -683,7 +683,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('yew-app-21b15578f3f117bb_bg.wasm', import.meta.url);
+        input = new URL('yew-app-de27c682bbf14392_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
