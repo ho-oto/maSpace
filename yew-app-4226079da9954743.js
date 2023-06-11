90bca782b3322c6bb7f88786edcf47658c793d2b
ho-oto <ho-oto@users.noreply.github.com>
2023-06-11 07:21:42 +0000

    deploy: 8d5fe54205a91eed8bde90899e8ae60a8b0f0997

--- yew-app-4226079da9954743.js
+++ yew-app-4226079da9954743.js
@@ -683,7 +683,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('yew-app-150086d498542fe9_bg.wasm', import.meta.url);
+        input = new URL('yew-app-4226079da9954743_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
